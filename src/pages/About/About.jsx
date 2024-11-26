5439666a993d54ec4495f4c0a830d5bb5e40ff48
Anushervon777 <anushervonhalimov416@gmail.com>
2024-11-27 01:06:58 +0500

    About

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,172 @@
 
 export default function About() {
   return (
+  <div className="p-10" style={{background: "rgba(19, 21, 23, 1)"}}>
+    <div className=" flex w-[100%] gap-6">
+      <div className="w-[56%]">
+        <h1 className="text-[55px]" style={{color: "rgba(247, 250, 255, 1)"}}>Join Us Today! <a style={{color: "rgba(207, 211, 218, 1)"}}>And Build
+        Your Best Career</a></h1>
+        <p className="font-mono text-gray-300">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
+        galley of type and scrambled it to make a type specimen book.</p>
+        <div className="bg-[rgba(19, 21, 23, 1)]">
+          <input type="text" style={{background:"rgba(100, 100, 100, 0.1)", border:"2px solid gray", }}  className="p-3 w-1/3 mr-10" placeholder="Search Location" name="" id="" />
+          <button style={{background:"rgba(27, 112, 241, 1)", borderRadius:"4px"}} className="p-3 w-1/6  text-white "   >Search</button>
+        </div>
+        <div className="flex justify-between max-w-[100%]" style={{marginTop:"50px"}}>
+        <div className="" style={{borderRight:"1px solid gray", width:"150px"}}>
+            <p className="text-5xl text-white font-semibold">10M +</p>
+            <p className="text-gray-300">Trusted Users</p>
+          </div>
+          <div style={{borderRight:"1px solid gray", width:"180px"}}>
+            <p className="text-5xl text-white font-semibold">₹ 20B +</p>
+            <p className="text-gray-300">Trusted Users</p>
+          </div>
+          <div style={{borderRight:"1px solid gray", width:"150px"}}>
+            <p className="text-5xl text-white font-semibold">10M +</p>
+            <p className="text-gray-300">Trusted Users</p>
+          </div>
+        </div>
+      </div>
+      <img src="/src/pages/About/aboutImg/3D illustration baru char lama (8) 1.png" style={{width:"54%"}} alt="" />
+    </div>
     <div>
-      
+    <div className="flex items-center justify-between mt-[100px]">
+      <p className="text-white text-4xl">Select Your Team For Job</p>
+      <input  type="text" placeholder="Search here" name="" id="" className="w-[310px] p-2" style={{background:"rgba(19, 21, 23, 1)", border:"2px solid gray "}} />
+      </div>
+      <div className="flex justify-center gap-5 pt-20">
+        <div>
+          <div className="p-2 w-[270px] text-gray-300 text-center" style={{border:"1px solid gray"}}>Legal (10)</div>
+          <div className="p-2 w-[270px] mt-5 text-gray-300 text-center" style={{border:"1px solid gray"}}>Legal (10)</div>
+          <div className="p-2 w-[270px] mt-5 text-gray-300 text-center" style={{border:"1px solid gray"}}>Legal (10)</div>
+         
+        </div>
+        <div>
+          <div className="p-2 w-[270px] text-gray-300 text-center" style={{border:"1px solid gray"}}>Legal (10)</div>
+          <div className="p-2 w-[270px] mt-5 text-gray-300 text-center" style={{border:"1px solid gray"}}>Legal (10)</div>
+          <div className="p-2 w-[270px] mt-5 text-gray-300 text-center" style={{border:"1px solid gray"}}>Legal (10)</div>
+         
+        </div>
+        <div>
+          <div className="p-2 w-[270px] text-gray-300 text-center" style={{border:"1px solid gray"}}>Legal (10)</div>
+          <div className="p-2 w-[270px] mt-5 text-gray-300 text-center" style={{border:"1px solid gray"}}>Legal (10)</div>
+          <div className="p-2 w-[270px] mt-5 text-gray-300 text-center" style={{border:"1px solid gray"}}>Legal (10)</div>
+         
+        </div>
+        <div>
+          <div className="p-2 w-[270px] text-gray-300 text-center" style={{border:"1px solid gray"}}>Legal (10)</div>
+          <div className="p-2 w-[270px] mt-5 text-gray-300 text-center" style={{border:"1px solid gray"}}>Legal (10)</div>
+          <div className="p-2 w-[270px] mt-5 text-gray-300 text-center" style={{border:"1px solid gray"}}>Legal (10)</div>
+         
+        </div>
+      </div>
+      <div>
+        <p className="text-center text-3xl text-white mt-16" >Open Job Roles</p>
+        <div className="flex items-center gap-6 justify-center p-1 w-[80%] m-auto mt-8 mb-8 h-[80px] " style={{border:"1px solid gray"}}>
+          <p className="text-gray-400 text-2xl w-[400px]">Executive Marketing Manager</p>
+            <p className="text-gray-400 text-1xl">Marketing | New Delhi, India | Full Time, Remote    </p>
+            <button className="text-blue-500 p-2 w-[140px]" style={{border:"2px solid rgba(27, 112, 241, 1)"}}>Apply now</button>
+          </div>
+          <div className="flex items-center gap-6 justify-center p-1 w-[80%] m-auto mt-8 mb-10 h-[80px]" style={{border:"1px solid gray"}}>
+          <p className="text-gray-400 text-2xl w-[400px]">Executive Marketing Manager</p>
+            <p className="text-gray-400 text-1xl">Marketing | New Delhi, India | Full Time, Remote    </p>
+            <button className="text-blue-500 p-2 w-[140px]" style={{border:"2px solid rgba(27, 112, 241, 1)"}}>Apply now</button>
+          </div>
+          <div className="flex items-center gap-6 justify-center p-1 w-[80%] m-auto mt-8 mb-8 h-[80px] " style={{border:"1px solid gray"}}>
+          <p className="text-gray-400 text-2xl w-[400px]">Executive Marketing Manager</p>
+            <p className="text-gray-400 text-1xl">Marketing | New Delhi, India | Full Time, Remote    </p>
+            <button className="text-blue-500 p-2 w-[140px]" style={{border:"2px solid rgba(27, 112, 241, 1)"}}>Apply now</button>
+          </div>
+          <div className="flex items-center gap-6 justify-center p-1 w-[80%] m-auto mt-8 mb-8 h-[80px] " style={{border:"1px solid gray"}}>
+          <p className="text-gray-400 text-2xl w-[400px]">Executive Marketing Manager</p>
+            <p className="text-gray-400 text-1xl">Marketing | New Delhi, India | Full Time, Remote    </p>
+            <button className="text-blue-500 p-2 w-[140px]" style={{border:"2px solid rgba(27, 112, 241, 1)"}}>Apply now</button>
+          </div>
+          <div className="flex items-center gap-6 justify-center p-1 w-[80%] m-auto mt-8 mb-8 h-[80px] " style={{border:"1px solid gray"}}>
+          <p className="text-gray-400 text-2xl w-[400px]">Executive Marketing Manager</p>
+            <p className="text-gray-400 text-1xl">Marketing | New Delhi, India | Full Time, Remote    </p>
+            <button className="text-blue-500 p-2 w-[140px]" style={{border:"2px solid rgba(27, 112, 241, 1)"}}>Apply now</button>
+          </div>
+          <div className="flex items-center gap-6 justify-center p-1 w-[80%] m-auto mt-8 mb-8 h-[80px] " style={{border:"1px solid gray"}}>
+          <p className="text-gray-400 text-2xl w-[400px]">Executive Marketing Manager</p>
+            <p className="text-gray-400 text-1xl">Marketing | New Delhi, India | Full Time, Remote    </p>
+            <button className="text-blue-500 p-2 w-[140px]" style={{border:"2px solid rgba(27, 112, 241, 1)"}}>Apply now</button>
+          </div>
+          <div className="flex items-center gap-6 justify-center p-1 w-[80%] m-auto mt-8 mb-8 h-[80px] " style={{border:"1px solid gray"}}>
+          <p className="text-gray-400 text-2xl w-[400px]">Executive Marketing Manager</p>
+            <p className="text-gray-400 text-1xl">Marketing | New Delhi, India | Full Time, Remote    </p>
+            <button className="text-blue-500 p-2 w-[140px]" style={{border:"2px solid rgba(27, 112, 241, 1)"}}>Apply now</button>
+          </div>
+          <div className="flex items-center gap-6 justify-center p-1 w-[80%] m-auto mt-8 mb-8 h-[80px] " style={{border:"1px solid gray"}}>
+          <p className="text-gray-400 text-2xl w-[400px]">Executive Marketing Manager</p>
+            <p className="text-gray-400 text-1xl">Marketing | New Delhi, India | Full Time, Remote    </p>
+            <button className="text-blue-500 p-2 w-[140px]" style={{border:"2px solid rgba(27, 112, 241, 1)"}}>Apply now</button>
+          </div>
+          <div className="flex items-center gap-6 justify-center p-1 w-[80%] m-auto mt-8 mb-8 h-[80px] " style={{border:"1px solid gray"}}>
+          <p className="text-gray-400 text-2xl w-[400px]">Executive Marketing Manager</p>
+            <p className="text-gray-400 text-1xl">Marketing | New Delhi, India | Full Time, Remote    </p>
+            <button className="text-blue-500 p-2 w-[140px]" style={{border:"2px solid rgba(27, 112, 241, 1)"}}>Apply now</button>
+          </div>
+          <div className="flex items-center gap-6 justify-center p-1 w-[80%] m-auto mt-8 mb-8 h-[80px] " style={{border:"1px solid gray"}}>
+          <p className="text-gray-400 text-2xl w-[400px]">Executive Marketing Manager</p>
+            <p className="text-gray-400 text-1xl">Marketing | New Delhi, India | Full Time, Remote    </p>
+            <button className="text-blue-500 p-2 w-[140px]" style={{border:"2px solid rgba(27, 112, 241, 1)"}}>Apply now</button>
+          </div>
+          <div className="flex justify-center items-center">
+          <button className="text-white p-3 bg-blue-500 w-[200px] text-center  " style={{justifyContent:"center"}}>Load More</button>
+
+          </div>
+
+      </div>
+    </div>
+    <div className="flex items-center justify-between p-3 h-[480px]" style={{background:'rgb(30 34 44)'}}>
+      <div className="w-[500px]">
+        <p className="text-white text-4xl font-semibold">Hurry Up! Apply Now Your Job Role</p>
+        <p className="font-mono text-gray-300 text-xs pt-8">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.</p>
+        <button className="text-white p-3 bg-blue-500 w-[200px] text-center mt-5 " style={{justifyContent:"center",borderRadius:"4px"}}>Apply Now</button>
+      </div>
+      <div><img src="/src/pages/About/aboutImg/work_from_home_for_landing_page_5_home_page_3d_rendering_Premium_Psd 2.png" className="" alt="" /></div>
+    </div>
+    <div>
+      <div className="text-center">
+      <h1 className="text-white text-4xl font-semibold">Frequently Asked Questions</h1>
+      <p className="font-mono text-gray-300 text-sm pt-3">When an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+      </div>
+      <div className="flex items-start justify-between mt-10" >
+        
+      <div className="w-[580px]">
+        <div className="p-3" style={{border:"1px solid gray"}}>
+          <h1 className="font-semibold text-xl text-white ">What is the difference between Defi and Metaverse ?</h1>
+          <p className="font-semibold text-xs text-gray-300">When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+        </div>
+        <div className="p-3 mt-5" style={{border:"1px solid gray"}}>
+          <h1 className="font-semibold text-xl text-white ">What should I do if Google verification fails to bind ?</h1>
+        </div>
+        <div className="p-3 mt-5" style={{border:"1px solid gray"}}>
+          <h1 className="font-semibold text-xl text-white ">How to complete your KYC Verification ?</h1>
+        </div>
+        <div className="p-3 mt-5" style={{border:"1px solid gray"}}>
+          <h1 className="font-semibold text-xl text-white ">What should I do if I enter the Google verification code incorrectly ?</h1>
+        </div>
+      </div>
+      <div>
+      <div className="p-3 mt-5" style={{border:"1px solid gray"}}>
+          <h1 className="font-semibold text-xl text-white ">What should I do if I enter the Google verification code incorrectly ?</h1>
+        </div>
+        <div className="p-3 mt-5" style={{border:"1px solid gray"}}>
+          <h1 className="font-semibold text-xl text-white ">What should I do if I enter the Google verification code incorrectly ?</h1>
+        </div>
+        <div className="p-3 mt-5" style={{border:"1px solid gray"}}>
+          <h1 className="font-semibold text-xl text-white ">What should I do if I enter the Google verification code incorrectly ?</h1>
+        </div>
+        <div className="p-3 mt-5" style={{border:"1px solid gray"}}>
+          <h1 className="font-semibold text-xl text-white ">What should I do if I enter the Google verification code incorrectly ?</h1>
+        </div>
+      </div>
+      </div>
+  
+
+    </div>
     </div>
   )
 }
