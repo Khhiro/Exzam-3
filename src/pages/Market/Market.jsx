4d8a7a3f35837013471dfc51a5d46375d8c31f65
Khhiro <yoabdurashid@gmail.com>
2024-11-27 00:33:59 +0500

    fgdsa

--- src/pages/Market/Market.jsx
+++ src/pages/Market/Market.jsx
@@ -5,7 +5,7 @@ export default function Market() {
   return (
     <div>
       {/* section1 */}
-      <div className="flex flex-wrap md:flex-nowrap">
+      <div className="flex flex-wrap md:flex-nowrap items-center md:justify-evenly">
         <div className="md:text-start text-center">
           <p className="text-[#1B70F1]">Crypto Brains</p><br />
           <h1 className="md:text-[50px] text-[35px] font-[900] text-white">Buy & Sell Crypto Easy <br />
@@ -18,7 +18,7 @@ export default function Market() {
           </div>
         </div>
         <div className="right">
-          <img src="./src/assets/img1-market.svg" alt="" />
+          <img width={500} src="./src/assets/img1-market.svg" alt="" />
         </div>
       </div>
     </div>
